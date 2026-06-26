4bb8892efff48205ac4a1c114e9b4aa63d7382ef
Emos Arenga <enos.arenga@gmail.com>
2026-06-26 19:15:53 +0300

    using nested functions and arrow functions to set login attempt

--- index.js
+++ index.js
@@ -1,5 +1,31 @@
+const userinfo = {
+  "username": "user1",
+  "password": "password1",
+  }
 
+  const createLoginTracker = (maxAttempts = 3) =>{
+    let attemptCount = 0;
+    return(userinfo) =>{
+    attemptCount++;
 
+    if(userinfo){
+      console.log("Login Successful");
+      attemptCount = 0;
+      return true;
+    }
+    if(attemptCount >= maxAttempts){
+      console.log("Account locked due to too many failed login attempt");
+      return false;
+    }
+    console.log(`Login failed, attempt ${attemptCount} of ${maxAttempts}`);
+    return false;
+  }
+}
+const login = createLoginTracker(3);
+login(null);
+login(null);
+login(null);
+login({userinfo:["user1"]});
 
 module.exports = {
   ...(typeof createLoginTracker !== 'undefined' && { createLoginTracker })
